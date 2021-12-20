5378801c44d67bf0468942a0f5c1eea3ed879f12
Aleksei Belousov <vs.oko@rambler.ru>
2021-12-20 16:15:29 +0300

    поменял тернарный ? на логический ||(добавил еще одно условие)

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -1,4 +1,4 @@
 function checkSpam(str) {
   // ваш код...
-  return  str.toLowerCase().includes('1xbet') || str.toLowerCase().includes('xxx') ? true : false
+  return str.toLowerCase().includes('1xbet') || str.toLowerCase().includes('xxx') || false;
 }
