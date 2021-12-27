2b4a21b4898bfc73861e2e2b36730f5c8d4ee11f
Aleksei Belousov <vs.oko@rambler.ru>
2021-12-27 17:57:41 +0300

    решил 3.5

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,5 @@
 function getMinMax(str) {
   // ваш код...
+  let arrStr = str.split(' ').filter(item => !isNaN(+item)).sort((a, b) => a - b);
+  return {'min': +arrStr[0], 'max': +arrStr[arrStr.length - 1]};
 }
