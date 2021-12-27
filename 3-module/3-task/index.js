2e12bbc56555a1ce8caef834f8153a61f1d78247
Aleksei Belousov <vs.oko@rambler.ru>
2021-12-27 16:31:43 +0300

    решил 3.3

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,9 @@
 function camelize(str) {
   // ваш код...
+  let result = '';
+  let spltedStr = str.split('-');
+  for (let i = 0; i < spltedStr.length; i++) {
+    result += i ? spltedStr[i].slice(0, 1).toUpperCase() + spltedStr[i].slice(1) : spltedStr[i];
+  }
+  return result;
 }
