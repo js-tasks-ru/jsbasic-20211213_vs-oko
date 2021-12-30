66fe83db76b9595d6ab59d72b2dfe4ee190a8cb2
Aleksei Belousov <vs.oko@rambler.ru>
2021-12-30 22:54:14 +0300

    решил 4.1

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,12 @@
 function makeFriendsList(friends) {
   // ваш код...
+  let ul = document.createElement('ul');
+  document.body.append(ul);
+  let friendsList = friends.map(item => item.firstName + ' ' + item.lastName);
+  for (let el of friendsList) {
+    let li = document.createElement('li');
+    li.textContent = el;
+    ul.append(li);
+  }
+  return ul;
 }
