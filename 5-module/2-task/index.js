24b125863ff807de7ab8a4d845879fbfbf433d7a
Aleksei Belousov <vs.oko@rambler.ru>
2022-01-05 19:46:29 +0300

    решил 5.2

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,9 @@
 function toggleText() {
   // ваш код...
+  let button = document.querySelector('.toggle-text-button');
+  let textId = document.getElementById('text');
+  button.addEventListener('click', reNewEl);
+  function reNewEl() {
+    return textId.hidden ? textId.hidden = false : textId.hidden = true;
+  }
 }
