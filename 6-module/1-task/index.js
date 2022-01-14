0dc1321633ec203c7b618d9307eca1b4e1575aac
Aleksei Belousov <vs.oko@rambler.ru>
2022-01-14 20:04:46 +0300

    6.1 решил

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -14,5 +14,48 @@
  */
 export default class UserTable {
   constructor(rows) {
+    this.elem = document.createElement('table');
+    this.renderThead();
+    this._renderTable(rows);
+    this.elem.addEventListener('click', () => {this._delRow(event);});
+  }
+
+  renderThead() {
+    let thead = document.createElement('thead');
+    let tr = document.createElement('tr');
+    this.elem.append(thead);
+    thead.append(tr);
+    let headTextArr = ['Имя', 'Возраст', 'Зарплата', 'Город', ''];
+    for (let key of headTextArr) {
+      let th = document.createElement('th');
+      tr.append(th);
+      th.append(key);
+    }
+  }
+
+  _renderTable(rows) {
+    let tbody = document.createElement('tbody');
+    this.elem.append(tbody);
+    for (let row of rows) {
+      let tr = document.createElement('tr');
+      tbody.append(tr);
+      for (let rowEl in row) {
+        let td = document.createElement('td');
+        tr.append(td);
+        td.append(row[rowEl]);
+      }
+      let td = document.createElement('td');
+      tr.append(td);
+      let button = document.createElement('button');
+      td.append(button);
+      button.append('X');
+    }
+
+  }
+
+  _delRow(event) {
+    //console.log(event.target);
+    if (event.target.textContent !== 'X') {return;}
+    event.target.closest('tr').remove();
   }
 }
